6750873471c94c2a236a0319bd02c81702ff176d
Simon Vrachliotis <simon@simonswiss.com>
2022-08-23 16:53:34 +1000

    Challenge 1 copy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colorThemes = require('./themes')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,md,mdx,js,jsx}'],
   theme: {
     extend: {
+      colors: {
+        base: colorThemes.base,
+        rainforest: colorThemes.rainforest,
+        candy: colorThemes.candy,
+      },
       fontFamily: {
         sans: ['Open Sans', 'sans-serif', ...defaultTheme.fontFamily.sans],
         mono: ['"Dank Mono"', ...defaultTheme.fontFamily.mono],
